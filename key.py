5bc079ddb1e77658e749f4b2aa9dbc39ef137b28
unknown <ayusdas508@gmail.com>
2025-07-12 18:43:36 +0530

    Des changes

--- key.py
+++ key.py
@@ -1,4 +1,2 @@
 import secrets
 print(secrets.token_hex(16))  # Copy this output
-#af86add7637610e66cb17564a92be561
-#postgresql://japan_journal_db_user:bor0AmvdrC8X1sd7BccCl8uNcVpaKQ5J@dpg-d1p5l3vfte5s73c01e2g-a.singapore-postgres.render.com/japan_journal_db
